5a53d0fbcb1f47d2c7661e7e6ad4cee8871ca458
Lucas Renard <lucas.renard83@gmail.com>
2025-11-21 16:03:29 +0100

    Job done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,30 +1,24 @@
 class BookmarksController < ApplicationController
-
   def new
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
-
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params.merge(list: @list))
     if @bookmark.save
-      redirect_to @list, notice: "Bookmark successfully created"
+      redirect_to @list, notice: "Bookmark created successfully"
     else
       render :new, status: :unprocessable_content
     end
   end
-
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path, status: :see_other
+    redirect_to list_path(@bookmark.movie), status: :see_other
   end
-
   private
-
-    def bookmark_params
-      params.require(:bookmark).permit(:movie_id, :list_id, :comment)
-    end
-
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :list_id, :comment)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,32 +1,30 @@
 class ListsController < ApplicationController
-
   def index
     @lists = List.all
   end
-
   def new
     @list = List.new
   end
-
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list, notice: "List successfuly created"
+        redirect_to @list, notice: "List created successfully"
     else
-      render :new, status: :unprocessable_content
+      render :new, status: :unprocessable_entity
     end
   end
-
   def show
     find_list
   end
-
-  private
-
-  def list_params
-    params.require(:list).permit(:name)
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
-
+  private
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
   def find_list
     @list = List.find(params[:id])
   end
